f16d8c2e4ecfa8f36b7ce8abe392c747596e1b0e
AlaEltigani <alaaltignai@hotmail.com>
2022-11-20 14:19:18 -0800

    t0.1

--- main.h
+++ main.h
@@ -1,2 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
 int _printf(const char *format, ...);
 Int _printf () ;
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+#endif
