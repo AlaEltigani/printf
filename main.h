db492b52ef8f989b7d94fa09981e7ddafae34449
AlaEltigani <alaaltignai@hotmail.com>
2022-11-22 10:02:17 -0800

    t0.3

--- main.h
+++ main.h
@@ -4,9 +4,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
-Int _printf () ;
-va_list list, char buffer[], int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
 #endif
